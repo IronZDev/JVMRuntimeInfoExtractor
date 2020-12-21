8934f58bbfee7c5f10db2ce1517676f91e42a75b
p.wolski@polsource.com <pa-wo97@o2.pl>
2020-12-21 15:24:22 +0100

    Add txt saver

--- src/main/java/com/mstokfisz/TxtSaver.java
+++ src/main/java/com/mstokfisz/TxtSaver.java
@@ -7,5 +7,14 @@ public class TxtSaver implements InfoSaver {
     @Override
     public void saveInformation(long freeMemory, long availableMemory, long availableProcessors) {
         // Your code here
+        try {
+            FileWriter file = new FileWriter("systemInfo.txt");
+            file.write("Free Memory: " + freeMemory + " MB\n");
+            file.write("Available Memory: " + availableMemory + " MB\n");
+            file.write("Available Processors: " + availableProcessors + "\n");
+            file.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
